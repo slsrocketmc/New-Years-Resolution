8370c50ce1e729856303f1f5d3c31ca5f1fb2985
River <slsrocketmc@gmail.com>
2026-08-17 17:57:09 -0400

    Basic landing page setup

---
+++ new-years-resolution/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NYR</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="parent">
+        <div class="content">
+            <div class="control">
+                <button type="button" aria-label="New">
+                    <img src="new-years-resolution/assets/new.svg" alt="New">
+                </button>
+            </div>
+            <div class="list"></div>
+            <div class="list"></div>
+        </div>
+    </div>
+</body>
+</html>
