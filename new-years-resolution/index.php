6859fce26178347e373e8b0bed54d6d16b5fb7f7
slsrocketmc <slsrocketmc@gmail.com>
2026-08-23 15:04:08 -0400

    Site Setup

--- new-years-resolution/index.php
+++ new-years-resolution/index.php
@@ -7,16 +7,15 @@
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <div class="parent">
-        <div class="content">
-            <div class="control">
-                <button type="button" aria-label="New">
-                    <img src="new-years-resolution/assets/new.svg" alt="New">
-                </button>
-            </div>
-            <div class="list"></div>
-            <div class="list"></div>
-        </div>
+<div class="content">
+    <div class="control">
+        <button type="button" aria-label="New" class="button">
+            
+            <img src="assets/new.svg" alt="New" width="30vw">
+        </button>
     </div>
+    <div class="list"></div>
+    <div class="list"></div>
+</div>
 </body>
 </html>
