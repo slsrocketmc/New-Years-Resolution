92f698dfe1923dbc284fdfb9d86158cc30f59cf5
River <slsrocketmc@gmail.com>
2026-08-27 17:16:55 +0000

    Started moving input menu to a new page, reverted many styling changes to main page.

--- new-years-resolution/index.php
+++ new-years-resolution/index.php
@@ -5,27 +5,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>NYR</title>
+    <link rel="stylesheet" href="styles/reset.css">
     <link rel="stylesheet" href="styles.css">
     <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon-temp/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon-temp/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon-temp/favicon-16x16.png">
     <link rel="manifest" href="/assets/favicon-temp/site.webmanifest">
-    <script>
+    <!--
+     <script>
         function toggleMenu() {
             const menu = document.getElementById("dropdown-menu");
             menu.classList.toggle("show");
         }
     </script>
+    -->
 </head>
 
 <body>
     <div class="content">
         <div class="control widget">
-            <button type="button" aria-label="New" class="button menu-toggle" onclick="toggleMenu()">
-
-                <img src="assets/new.svg" alt="New" width="30vw">
-            </button>
-            <nav id="dropdown-menu" class="hidden-menu">
+            <a href="input.html">
+                <div class="button">
+                    <img src="assets/new.svg" alt="New" width="30vw">
+                </div>
+            </a>
+            <!-- <button type="button" aria-label="New" class="button"> -->
+            <!-- </button> -->
+            <!-- <nav id="dropdown-menu" class="hidden-menu">
                 <form action="process.php" method="post" target="_blank" class="input-form">
                     <input type="text" name="title" id="title" placeholder="Title">
                     <input type="text" name="description" id="description" placeholder="Description">
@@ -38,7 +44,7 @@
                         <input type="submit" value="Submit">
                     </div>
                 </form>
-            </nav>
+            </nav> -->
         </div>
         <div class="list widget">
 
