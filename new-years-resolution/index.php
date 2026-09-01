9bfa2267ac9fbae42036357e7ebb9836943ad81a
River <slsrocketmc@gmail.com>
2026-09-01 16:50:33 +0000

    Changed styling on input page & fixed certain css issues

--- new-years-resolution/index.php
+++ new-years-resolution/index.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>NYR</title>
-    <link rel="stylesheet" href="styles/reset.css">
     <link rel="stylesheet" href="styles.css">
     <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon-temp/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon-temp/favicon-32x32.png">
